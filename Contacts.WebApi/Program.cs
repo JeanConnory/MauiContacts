a07e6f35ed84e05870c47cf9cfcbc9b82c472f02
Jean Connory <michaelrhcp@gmail.com>
2023-04-09 13:29:34 -0300

    creating webApi and connecting with sqlite database

---
+++ Contacts.WebApi/Program.cs
@@ -0,0 +1,17 @@
+using Contacts.WebApi;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+	option.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.Run();
