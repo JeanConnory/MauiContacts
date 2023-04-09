3980d844dd3b1c6472b49f61756d8b5b57298651
Jean Connory <michaelrhcp@gmail.com>
2023-04-09 14:02:22 -0300

    crud webApi completed

--- Contacts.WebApi/Program.cs
+++ Contacts.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Contacts.WebApi;
+using Contacts.WebApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,48 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
+app.MapGet("/api/contacts", async (ApplicationDbContext db) =>
+{
+	var contacts = await db.Contacts.ToListAsync();
+	return Results.Ok(contacts);
+});
+
+app.MapPost("/api/contacts", async (Contact contact, ApplicationDbContext db) =>
+{
+	db.Contacts.Add(contact);
+	await db.SaveChangesAsync();
+});
+
+app.MapPut("/api/contacts/{id}", async (int id, Contact contact, ApplicationDbContext db) =>
+{
+	var contactToUpdate = await db.Contacts.FindAsync(id);
+
+	if(contactToUpdate is null) return Results.NotFound();
+
+	contactToUpdate.Name = contact.Name;
+	contactToUpdate.Email = contact.Email;
+	contactToUpdate.Phone = contact.Phone;
+	contactToUpdate.Address = contact.Address;
+
+	await db.SaveChangesAsync();
+	return Results.NoContent();
+
+});
+
+app.MapDelete("/api/contacts/{id}", async (int id, ApplicationDbContext db) =>
+{
+	var contactToDelete = await db.Contacts.FindAsync(id);
+
+	if(contactToDelete != null)
+	{
+		db.Contacts.Remove(contactToDelete);
+		await db.SaveChangesAsync();
+		return Results.Ok(contactToDelete);
+	}
+
+	return Results.NotFound();
+});
 
 app.Run();
